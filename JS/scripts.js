5f5e13e0af971442634a4dbe1a1b254ff9b542fe
annajons <annajonsdottir91@gmail.com>
2017-02-06 12:28:18 -0800

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,34 @@
+var showHide = true;
+
+function showToggle() {
+  if (showHide === false) {
+    //document.getElementById('sideMenu').style.display = 'block';
+    document.getElementById('sideMenu').classList.toggle('show');
+    showHide = true;
+  }
+  else {
+    document.getElementById('sideMenu').classList.toggle('show');
+    //document.getElementById('sideMenu').style.display = 'none';
+    showHide = false;
+  }
+}
+
+function showDropdown(inOut) {
+  // Show dropdown
+  if (inOut === 0) {
+    document.getElementById('dropdownMenu').style.display = 'block';
+  }
+  else {
+    document.getElementById('dropdownMenu').style.display = 'none';
+  }
+}
+
+function switchContent(page)  {
+  var pages = document.getElementsByClassName('contentPage');
+
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].style.display = 'none';
+  };
+
+  document.getElementById(page).style.display = 'block';
+}
